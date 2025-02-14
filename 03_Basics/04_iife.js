55fae09ee6ce0b5f8859d337926169740803ca10
anish8346 <kumaranish68346@gmail.com>
2025-02-14 11:36:40 +0530

    IIfe

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immmediately Invoked Function Expression (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNCETED`);
+}) ();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )( 'ANISH')
