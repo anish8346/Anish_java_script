b72d06c28efa7aee355953b897819f838aa0684f
anish8346 <kumaranish68346@gmail.com>
2025-02-17 16:51:20 +0530

    control flow

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+
+    case "Feb":
+        console.log("Febuary");
+        break;
+
+    case "march":
+        console.log("March");
+        break;
+
+    case "April" :
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+        
+        
+}
